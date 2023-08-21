8bb072fd889903940adaf08fa997a08544ec11d1
Harriet O'Brien <42326843+harrietobrien@users.noreply.github.com>
2023-08-20 20:10:20 -0400

    setting up home buttons

--- main.py
+++ main.py
@@ -6,27 +6,46 @@ import pygame_gui
 
 
 class RunGame:
+    BLACK = pygame.Color('#000000')
+
     def __init__(self, width, height):
         self.width = width
         self.height = height
-        self.bkgd_color = pygame.Color('#000000')
+        self.buttons = list()
+        self.init_ui()
+        self.game_loop()
 
+    def set_buttons(self):
+        def offset_button(top_left):
+            padding = 80
+            top_left[1] += padding
+            print(top_left)
+            return top_left
+
+        btn_width, btn_height = 100, 50
+        top_left = [self.width//2 - btn_width//2, btn_height]
+        dimensions = [btn_width, btn_height]
+
+        self.button = pygame.Rect(top_left, dimensions)
+        self.btn_names = ['START', 'SETTINGS', 'OTHER']
+        for btn_name in self.btn_names:
+            print(top_left, dimensions)
+            btn = pygame_gui.elements.UIButton(relative_rect=self.button,
+                                               text=btn_name,
+                                               manager=self.manager)
+            self.buttons.append(btn)
+            self.button = pygame.Rect(offset_button(top_left), dimensions)
+
+    def init_ui(self):
         pygame.init()
-
         pygame.display.set_caption('Quick Start')
         self.window_surface = pygame.display.set_mode((self.width, self.height))
-
         self.background = pygame.Surface((self.width, self.height))
-        #
-        self.background.fill(self.bkgd_color)
+        self.background.fill(RunGame.BLACK)
 
         self.manager = pygame_gui.UIManager((self.width, self.height))
 
-        self.rectangle = pygame.Rect((350, 275), (100, 50))
-        self.hello_button = pygame_gui.elements.UIButton(relative_rect=self.rectangle,
-                                                         text='Say Hello',
-                                                         manager=self.manager)
-        self.game_loop()
+        self.set_buttons()
 
     def game_loop(self):
         clock = pygame.time.Clock()
@@ -39,8 +58,9 @@ class RunGame:
                     is_running = False
 
                 if event.type == pygame_gui.UI_BUTTON_PRESSED:
-                    if event.ui_element == self.hello_button:
-                        print('Hello World!')
+                    for btn in self.buttons:
+                        if event.ui_element == btn:
+                            print('Hello World!')
 
                 self.manager.process_events(event)
 
