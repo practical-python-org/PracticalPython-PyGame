7a348832bade84bbb4b6c0cefd4061484fb955ee
JefeThePug <107787719+JefeThePug@users.noreply.github.com>
2023-08-21 15:01:56 +0900

    common.py addition and changes to main.py
    Added common.py to store Global variables which can be shared across modules. (screen height/width, fps, etc).
    
    Modified bits to the RunGame class including adding a get_events method which gathers common pygame events and stores them in a dictionary.  This dictionary can then be passed to other classes/modules/etc to read the events as required.  This should prevent any double calling of pygame.event.get() as calling this twice in one iteration does not yield desireable results.
    
    Finally I shortened the alias of pygame in the import to the standard "pg" to make things shorter.

---
+++ common.py
@@ -0,0 +1,17 @@
+import pygame
+
+# Colors
+COLORS = {
+          "black": pygame.Color('#000000'), 
+}
+
+# Tiles and Screen
+TILE_SIZE = 64
+VERTICAL_TILE_NUM = 10
+HORIZONTAL_TILE_NUM = 12
+
+HEIGHT = VERTICAL_TILE_NUM * TILE_SIZE
+WIDTH = HORIZONTAL_TILE_NUM * TILE_SIZE
+
+# Time
+FPS = 60
